bcb605e7f721284e6c445bbf180ea1246b467331
Adam Bowles <github@adambowl.es>
2023-02-25 17:23:00 +0000

    Initial Prettier format

--- src/configuration.js
+++ src/configuration.js
@@ -1,14 +1,21 @@
-import { Type, getValidator, defaultAppConfiguration } from '@feathersjs/typebox'
+import {
+  Type,
+  getValidator,
+  defaultAppConfiguration,
+} from '@feathersjs/typebox';
 
-import { dataValidator } from './validators.js'
+import { dataValidator } from './validators.js';
 
 export const configurationSchema = Type.Intersect([
   defaultAppConfiguration,
   Type.Object({
     host: Type.String(),
     port: Type.Number(),
-    public: Type.String()
-  })
-])
+    public: Type.String(),
+  }),
+]);
 
-export const configurationValidator = getValidator(configurationSchema, dataValidator)
+export const configurationValidator = getValidator(
+  configurationSchema,
+  dataValidator
+);
--- src/validators.js
+++ src/validators.js
@@ -1,5 +1,5 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/validators.html
-import { Ajv, addFormats } from '@feathersjs/schema'
+import { Ajv, addFormats } from '@feathersjs/schema';
 
 const formats = [
   'date-time',
@@ -15,14 +15,14 @@ const formats = [
   'uri-template',
   'json-pointer',
   'relative-json-pointer',
-  'regex'
-]
+  'regex',
+];
 
-export const dataValidator = addFormats(new Ajv({}), formats)
+export const dataValidator = addFormats(new Ajv({}), formats);
 
 export const queryValidator = addFormats(
   new Ajv({
-    coerceTypes: true
+    coerceTypes: true,
   }),
   formats
-)
+);
