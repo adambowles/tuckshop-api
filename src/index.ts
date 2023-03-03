78956fc8e9a1cecbd6de0ab15c2a69962a4c3b0e
Adam Bowles <github@adambowl.es>
2023-03-03 16:49:09 +0000

    Don't need to know public IP any more

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 require('dotenv').config();
 
-const { exec } = require('child_process');
-
-exec('curl ip-adresim.app', function (error: any, stdout: any, stderr: any) {
-  if (error) return;
-  console.log('your ip is: ' + stdout);
-});
-
 import { app } from './app';
 import { logger } from './logger';
 
